232e658883b584cc0ccf99ec40f9c68977e1307e
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-02-25 23:53:04 +0900

    add installation textbox of snapshot

--- src/pages/snapshots.js
+++ src/pages/snapshots.js
@@ -2,13 +2,41 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import { getSnapshotPath } from "../components/common"
-import { Table, Row, Col } from 'react-bootstrap'
+import { Table, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+function CommandLineItem(props) {
+	if (!props.value) return (null);
+
+	return (
+    <div>
+      <p>
+				<InputGroup className="mb-3">
+					<FormControl
+						aria-label="Install command"
+						aria-describedby="basic-addon2"
+						value={props.value}
+						readonly
+					/>
+					<InputGroup.Append>
+            <CopyToClipboard text={props.value}>
+              <Button variant="outline-secondary">Copy</Button>
+            </CopyToClipboard>
+					</InputGroup.Append>
+				</InputGroup>
+      </p>
+    </div>
+	)
+}
 
 function SnapshotTable(props) {
   return (
     <Row className="mx-auto my-3">
       <Col>
         <h3>{props.title}</h3>
+        <CommandLineItem
+          value={`opam install satyrographos-snapshot-${props.title} && satyrographos install`}
+        />
         <Table>
           <thead>
             <tr>
