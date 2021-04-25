6c1a55301b38fcafdef3c64be5eaac802f9e5069
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-04-26 00:41:06 +0900

    add command name extraction script

---
+++ scripts/add-command-info.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'pathname'
+
+db_path = ARGV[0]
+db = JSON.parse(File.read(db_path))
+package_root_path = ARGV[1]
+
+db.each do |p|
+  inline-commands = []
+  block-commands = []
+  math-commands = []
+
+  package_path = Pathname(package_root_path)) / p['name']
+  if Dir.exists?(package_path) then
+    `grep -rn `
+  end
+
+  latest['inline-commands'] = inline-commands
+  latest['block-commands'] = block-commands
+  latest['math-commands'] = math-commands
+end
+
+File.open(db_path, 'w') do |f|
+  f.write(JSON.pretty_generate(db))
+end
