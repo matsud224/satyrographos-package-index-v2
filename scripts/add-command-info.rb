5406e8636ee43f8d7526fa744dadf437316b6e4b
Daiki Matsunaga <matsu.d.224@icloud.com>
2021-04-26 09:50:32 +0900

    fix

--- scripts/add-command-info.rb
+++ scripts/add-command-info.rb
@@ -12,17 +12,18 @@ db.each do |p|
   block_commands = []
   math_commands = []
 
-  package_path = Pathname(package_root_path) / p['name']
+  package_path = Pathname(package_root_path) / (p['name'].delete_prefix('satysfi-'))
   if Dir.exists?(package_path) then
     inline_commands = `grep -Rh let-inline #{package_path} | grep -ho '\\\\[0-9a-zA-Z-]*'`.split(/\R/)
     block_commands = `grep -Rh let-block #{package_path} | grep -ho '\\+[0-9a-zA-Z-]*'`.split(/\R/)
     inline_math = `grep -Rh let-math #{package_path} | grep -ho '\\\\[0-9a-zA-Z-]*'`.split(/\R/)
   end
 
-  latest = p['versions'][0]
-  latest['inline-commands'] = inline_commands
-  latest['block-commands'] = block_commands
-  latest['math-commands'] = math_commands
+  p['versions'].each do |v|  # FIXME: per-version
+    v['inline-commands'] = inline_commands
+    v['block-commands'] = block_commands
+    v['math-commands'] = math_commands
+  end
 end
 
 File.open(db_path, 'w') do |f|
